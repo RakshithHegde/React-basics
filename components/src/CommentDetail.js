caa6c323e06c9d6372a75bcae44bbc5755e55f64
Rakshith hegde <rakshithhgd@gmail.com>
2021-07-11 19:16:19 +0530

    11-07

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import faker from 'faker';
 
 
-const CommentDetail = () => {
+
+const CommentDetail = (props) => {
+    //console.log(props);
     return (
         <div className="comment">
         <a href="/" className="avatar">
-            <img src={faker.image.avatar()} alt="avatar" />
+            <img src={props.userpic} alt="avatar" />
         </a>
         <div className="content">
-            <a href="/" className="author">
-                Sam
-            </a>
+            <a href="/" className="author">  {/* basically props is an argument(object) that will pass the data from this file tp index file*/}
+                {props.user}  {/*the user is a reference that will take the input value and display it in browser */}
+            </a> 
             <div className="metadata">
-                <span className="date">Today at 6pm</span>
+                <span className="date">{props.time}</span>
             </div>
-            <div className="text">Nice post!</div>
+            <div className="text">{props.comment}</div>
         </div>
     </div>
     );
--- components/src/index.js
+++ components/src/index.js
@@ -1,52 +1,35 @@
-import React from 'react';
-import ReactDOM  from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={faker.image.avatar()} alt="avatar" />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6pm</span>
-                    </div>
-                    <div className="text">Nice post!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src={faker.image.avatar()} alt="avatar" />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6pm</span>
-                    </div>
-                    <div className="text">Nice post!</div>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        user="Jen"
+        time="Today at 9:40pm"
+        comment="nice post!"
+        userpic={faker.image.people()}
+      />
+      <CommentDetail
+        user="Alex"
+        time="yesterday at 2:40pm"
+        comment="amazing!"
+        userpic={faker.image.animals()}
+      />
+      <CommentDetail
+        user="Luke"
+        time="Today at 6:00am"
+        comment="Damn!!"
+        userpic={faker.image.nature()}
+      />
+    </div>
+  );
 };
-if(module.hot) {
-    module.hot.accept();
-  }
 
+if (module.hot) {
+  module.hot.accept();
+}
 
-ReactDOM.render(<App />,document.querySelector('#root'));
-
-
-
-
-
-
-
+ReactDOM.render(<App />, document.querySelector("#root"));
