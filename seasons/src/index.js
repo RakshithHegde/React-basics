6032d13e0cf2f75f72f8666d0c1f78cf1b00e794
Rakshith hegde <rakshithhgd@gmail.com>
2021-07-15 19:40:33 +0530

    15-07

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,14 +3,21 @@ import ReactDOM  from 'react-dom';
 //import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {lat: null};
+    }
+
+    //render should be defined
     render() {
         window.navigator.geolocation.getCurrentPosition(
             position => console.log(position),
             error => console.log(error)
         );
-        return <div>Latitude:</div>;
+        return <div style={{backgroundColor:'crimson'}}>Latitude:</div>;
     }
-}
+};
 
 
 if(module.hot) {
