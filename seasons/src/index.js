e75ab6b388ec62a76ca89fa3bdce810051b52f26
Rakshith hegde <rakshithhgd@gmail.com>
2021-07-15 19:14:13 +0530

    15-07

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,18 +2,17 @@ import React from 'react' ;
 import ReactDOM  from 'react-dom';
 //import SeasonDisplay from './SeasonDisplay';
 
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            error => console.log(error)
+        );
+        return <div>Latitude:</div>;
+    }
+}
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        error => console.log(error)
-    );
 
-
-    return (
-        <div>Latitude</div>
-    );
-};
 if(module.hot) {
     module.hot.accept();
 }
