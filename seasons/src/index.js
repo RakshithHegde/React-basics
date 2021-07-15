67961bf11c0ae30ffb34ed8f94cd5c882aeb3b32
Rakshith hegde <rakshithhgd@gmail.com>
2021-07-15 21:19:56 +0530

    15-07

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,34 +5,37 @@ import ReactDOM  from 'react-dom';
 class App extends React.Component {
     constructor(props) {
         super(props);
-        //this is only time we assign a value
-        this.state = {lat: null,long: null};
+        //this is only time we assign a value directly
+        this.state = {lat: null,errorMessage:""};
 
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 //to update state object  we called setState
-                this.setState({lat: position.coords.latitude,long: position.coords.longitude});
+                this.setState({lat: position.coords.latitude});
 
                 //this is wrong => this.state.lat = position.coords.latitude
 
             },
-            error => console.log(error)
+            error => {
+                this.setState({errorMessage: error.message})
+            }
         );
     }
 
     //render should be defined
     render() {
-  
-        return <div style={{backgroundColor:'maroon'}}>
-            Latitude: {this.state.lat} <br /> 
-            Longitude: {this.state.long}
-
-        </div>;
+        if(this.state.errorMessage && !this.state.lat){
+            return <div style={{backgroundColor:'maroon'}}>Error: {this.state.errorMessage}</div>
+        }
+        if(!this.state.errorMessage && this.state.lat){
+            return <div style={{backgroundColor:'orange'}}>Latitude: {this.state.lat}</div>
+        }
+        return <div>Loading!</div>;
     }
 };
 
-
+//for autoreloading when the file is saved
 if(module.hot) {
     module.hot.accept();
 }
