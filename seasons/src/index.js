34272154a63e3cc0893f17e65214bfc17832a2a9
Rakshith hegde <rakshithhgd@gmail.com>
2021-07-15 20:04:48 +0530

    15-07

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,17 +5,30 @@ import ReactDOM  from 'react-dom';
 class App extends React.Component {
     constructor(props) {
         super(props);
+        //this is only time we assign a value
+        this.state = {lat: null,long: null};
 
-        this.state = {lat: null};
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                //to update state object  we called setState
+                this.setState({lat: position.coords.latitude,long: position.coords.longitude});
+
+                //this is wrong => this.state.lat = position.coords.latitude
+
+            },
+            error => console.log(error)
+        );
     }
 
     //render should be defined
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            error => console.log(error)
-        );
-        return <div style={{backgroundColor:'crimson'}}>Latitude:</div>;
+  
+        return <div style={{backgroundColor:'maroon'}}>
+            Latitude: {this.state.lat} <br /> 
+            Longitude: {this.state.long}
+
+        </div>;
     }
 };
 
