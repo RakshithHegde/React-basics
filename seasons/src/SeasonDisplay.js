758c9e1bebd39e374c4069078238a06ddeddc28e
Rakshith hegde <rakshithhgd@gmail.com>
2021-07-25 12:10:24 +0530

    25-07

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,16 @@
 import React from "react";
+import "./SeasonDisplay.css";
+
+const seasonConfig = {
+  summer: {
+    text: "Summertime means it's icecream time" ,
+    iconName : 'sun'
+  },
+  winter: {
+    text: "Winter is coming",
+    iconName : 'snowflake'
+  }
+};
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -11,14 +23,15 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   //console.log(props.lat);
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === "winter" ? "Its chilly out here" : "Its beach time";
-  const icon = season === "winter" ? "snowflake" : "sun";
+  const {text, iconName} = seasonConfig[season]  //{text, iconName}
+ // const text = season === "winter" ? "Its chilly out here" : "Its beach time";
+  //const icon = season === "winter" ? "snowflake" : "sun";
   //console.log(season);
   return (
-    <div>
-      <i className={`${icon} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h2>{text}</h2>
-      <i className={`${icon} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react' ;
 import ReactDOM  from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+import './index.css'
 
 class App extends React.Component {
    /* constructor(props) {
@@ -19,19 +21,22 @@ class App extends React.Component {
             error => this.setState({errorMessage: error.message})
         );
     }
-    componentDidUpdate() {
-        console.log("My component was just updated - it rerendered");
-    }
-
-    //render should be defined
-    render() {
+    renderContent() {  //helper method
         if(this.state.errorMessage && !this.state.lat){
             return <h3 style={{backgroundColor:'maroon'}}>Error❎:  {this.state.errorMessage}</h3>
         }
         if(!this.state.errorMessage && this.state.lat){
             return <SeasonDisplay lat={this.state.lat}/> 
         }
-        return <h3>Loading!🔄 </h3>;
+        return <Spinner message={'Please accept location request'} />;
+    }
+    //render should be defined
+    render() {
+        return (
+            <div className="border">
+                {this.renderContent()}
+            </div>
+        )
     }
 };
 
