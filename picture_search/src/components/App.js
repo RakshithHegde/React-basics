7b5f188507126d7ab45f9d8fcf5cdc64c1f01a8b
Rakshith <rakshithhgd@gmail.com>
2021-08-23 09:47:45 +0530

    23-08

--- picture_search/src/components/App.js
+++ picture_search/src/components/App.js
@@ -1,20 +1,27 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
-  }
-  
-  render(){
+    //console.log(term);
+    axios.get('https://api.unsplash.com/search/photos',{
+      params: {
+        query: term
+      },
+      headers:{
+        Authorization: 'Client-ID b8CnbFIPVn-LDyLjtjbCpoMgtNDtBBFwp_NIHprR3uQ'
+      }
 
-  
-  return (
-    <div className="ui container" style={{marginTop: '10px'}}>
-      <SearchBar onSubmit={this.onSearchSubmit} />
-    </div>
-  );
-};
+    });
+  }
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
