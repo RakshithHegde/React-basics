5bec90d96670e629d1d151e3a91673e3a3945337
Rakshith <rakshithhgd@gmail.com>
2021-08-25 12:11:42 +0530

    25-08

--- video_search/src/components/App.js
+++ video_search/src/components/App.js
@@ -6,6 +6,10 @@ import VideoDetail from "./VideoDetail";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+      this.onTermSubmit('Supercars');
+  }
+
   onTermSubmit = async (term) => {
     //console.log(term);
     const response = await youtube.get("/search", {
@@ -14,7 +18,10 @@ class App extends React.Component {
       },
     });
     // console.log(response);
-    this.setState({ videos: response.data.items });
+    this.setState({
+         videos: response.data.items,
+         selectedVideo: response.data.items[0]
+        });
   };
   onVideoSelect = (video) => {
     //console.log("from the app", video);
