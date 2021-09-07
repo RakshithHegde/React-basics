85711e59c5ca6bc11fc2a42f5d93d7e09f834a2d
Rakshith <rakshithhgd@gmail.com>
2021-09-07 19:34:32 +0530

    widgets

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
-  return <h1>Search</h1>;
+  const [term, setTerm] = useState("");
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="feild">
+          <label>Enter search term</label>
+          <input
+            type="text"
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
