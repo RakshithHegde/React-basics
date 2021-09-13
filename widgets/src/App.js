58fa61f8e8c8026191c0c910880860407b1d656a
Rakshith <rakshithhgd@gmail.com>
2021-09-13 11:39:54 +0530

    widgets

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -17,36 +18,11 @@ const items = [
     content: "you use react by creating components",
   },
 ];
-const options = [
-  {
-    label: "The color red",
-    value: "red",
-  },
-  {
-    label: "The color green",
-    value: "green",
-  },
-  {
-    label: "The color blue",
-    value: "blue",
-  },
-];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown ? (
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        />
-      ) : null}
+      <Translate />
     </div>
   );
 };
